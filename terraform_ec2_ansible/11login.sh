9efea2f18e28504e78376a1d019966fa9fcf4cb0
sripaljain <sripal.jain@gmail.com>
2021-06-04 12:32:47 +0800

    aniblecopy2remote

--- terraform_ec2_ansible/11login.sh
+++ terraform_ec2_ansible/11login.sh
@@ -1,3 +1,4 @@
+# http://www.inanzzz.com/index.php/post/6138/setting-up-a-nginx-docker-container-on-remote-server-with-ansible
 ip=54.179.98.255
 ssh -i vamakp.pem ubuntu@${ip}
 # ansible-playbook  -i 54.179.98.255, --private-key ./vamakp.pem nginx.yaml
